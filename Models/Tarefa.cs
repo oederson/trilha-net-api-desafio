bef36e6154f9847834bdaa43fabaebd27fb3f72f
oederson <eder.hu@live.com.pt>
2023-10-18 03:43:11 -0300

    Data anotations

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -7,10 +7,13 @@ namespace TrilhaApiDesafio.Models
         [Key]
         [Required]
         public int Id { get; set; }
+        [Required(ErrorMessage ="Necessario informar o tiulo")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage ="Necessario informar a descrição")]
         public string Descricao { get; set; }
         [Required(ErrorMessage ="Data requerida")]
         public DateTime Data { get; set; }
+        [Required(ErrorMessage ="Necessario informar o status")]
         public EnumStatusTarefa Status { get; set; }
     }
 }
