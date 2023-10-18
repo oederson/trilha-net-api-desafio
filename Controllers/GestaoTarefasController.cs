aa2ee0a60346262417bc8f7a929797e4d036b227
oederson <eder.hu@live.com.pt>
2023-10-18 03:35:22 -0300

    padronização DTO

---
+++ Controllers/GestaoTarefasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrilhaApiDesafio.Context;
+using TrilhaApiDesafio.Models;
+
+namespace TrilhaApiDesafio.Controllers
+{
+    public class GestaoTarefasController : Controller
+    {
+        private readonly OrganizadorContext _db;
+        public GestaoTarefasController(OrganizadorContext db)
+        {
+            _db = db;            
+        }
+        public IActionResult Index()
+        { 
+
+            IEnumerable<Tarefa> tarefas = _db.Tarefas;
+            return View(tarefas);
+        }
+        public IActionResult Cadastrar() 
+        {
+            return View();
+        }
+    }
+}
